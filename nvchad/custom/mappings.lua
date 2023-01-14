82f32335c493e3c79ff08ab71d3363155b230588
asrul10 <asrul@kotaxdev.com>
2023-01-14 21:40:49 +0700

    fix: better find and replace selected items

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -3,12 +3,12 @@ local M = {}
 M.general = {
 	n = {
 		["<leader>sa"] = { ":%s/", "Find and replace all" },
-		["<leader>ss"] = { ":.,.+", "Find and replace current cursor to ++" },
 		["<leader>ft"] = { ":TodoTelescope previewer=false<CR>", "Find TODO" },
 	},
 	v = {
 		["J"] = { ":m '>+1<CR>gv=gv", "move line down" },
 		["K"] = { ":m '<-2<CR>gv=gv", "move line up" },
+    ["<leader>ss"] = { ":s/", "Find and replace selected lines" },
 	},
 }
 
