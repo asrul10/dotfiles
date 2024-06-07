a4029a9d320b5ff113094d04047caa2662a9022a
asrul10 <asrul@kotaxdev.com>
2024-06-08 06:03:16 +0700

    feat: update comment plugins

--- nvchad/nvim/lua/mappings.lua
+++ nvchad/nvim/lua/mappings.lua
@@ -13,7 +13,6 @@ map(
   "<cmd>lua require('readable-number').readable_number()<CR>",
   { desc = "Convert number to readable" }
 )
-map("n", "gcc", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", { desc = "toggle comment" })
 map("n", "<leader>kk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
 map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Open line diagnostics" })
 map("n", "<leader>fp", "<cmd>Copilot panel<CR>", { desc = "Show copilot panel" })
@@ -21,12 +20,6 @@ map("n", "<leader>fp", "<cmd>Copilot panel<CR>", { desc = "Show copilot panel" }
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 map("v", "<leader>ss", ":s/", { desc = "Find and replace selected lines" })
-map(
-  "v",
-  "gcc",
-  "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-  { desc = "Toggle comment" }
-)
 
 map("n", "]c", "<cmd>Gitsigns next_hunk<CR>", { desc = "Next hunk" })
 map("n", "[c", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Previous hunk" })
