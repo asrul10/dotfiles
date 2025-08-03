46c8545b4d04a7caae6c55fb4ffa038ce5586f41
asrul10 <asrul@kotaxdev.com>
2025-08-03 07:39:39 +0700

    fix(plugins): update treesj configuration
    Refactor the configuration for the treesj plugin to use opts
    instead of the previous config function. This simplifies
    initialization and improves compatibility with other plugins.

--- nvchad/nvim/lua/plugins/init.lua
+++ nvchad/nvim/lua/plugins/init.lua
@@ -44,9 +44,7 @@ return {
     "Wansmer/treesj",
     keys = { "<space>m", "<space>j", "<space>s" },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("treesj").setup()
-    end,
+    opts = {},
   },
 
   {
